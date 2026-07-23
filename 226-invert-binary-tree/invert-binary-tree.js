8a615c57c043e6484da88a343cdc3b6de9a20ec7
Kajal1395 <60035417+Kajal1395@users.noreply.github.com>
2026-07-23 22:05:25 +0530

    Time: 0 ms (100.00%) | Memory: 55.5 MB (5.53%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.js
+++ 226-invert-binary-tree/invert-binary-tree.js
@@ -11,13 +11,11 @@
  * @return {TreeNode}
  */
 var invertTree = function (root) {
-    function invert(root) {
-        if (!root) return
-        [root.left, root.right] = [root.right, root.left]
-        invertTree(root.left)
-        invertTree(root.right)
-    }
-    invert(root)
+    if (!root) return null;
+    [root.left, root.right] = [root.right, root.left]
+    invertTree(root.left)
+    invertTree(root.right)
+
     return root
 
 };
