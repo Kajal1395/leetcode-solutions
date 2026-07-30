df00814c009329663d4b4589654c6c115f238676
Kajal1395 <60035417+Kajal1395@users.noreply.github.com>
2026-07-30 18:47:07 +0530

    Time: 137 ms (9.26%) | Memory: 91.7 MB (14.81%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.js
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.js
@@ -4,22 +4,30 @@
  * @return {number[]}
  */
 var maximumBeauty = function (items, queries) {
-    items.sort((a, b) => a[0] - b[0])
-    let maxi = items[0][1]
-    for (let i = 1; i < items.length; i++) {
-        maxi = Math.max(items[i][1], maxi)
-        items[i][1] = maxi
-    }
+    //sort by price if two price same then by beauty [[1,2],[2,4],[3,2],[3,5],[5,6]]
+    //update beauty to maxbeauty till current position  can remove duplicate [[1,2],[2,4],[3,5],[5,6]]
+    //res=[2,4,5,5,6,6]  
+    items.sort((a, b) => {
+        if (a[0] === b[0]) {
+            return a[1] - b[1]
+        }
+        return a[0] - b[0]
+    })
+    let maxPrefix = -Infinity
+    let prefixArr = items.map(item => {
+        maxPrefix = Math.max(maxPrefix, item[1]);
+        return [item[0], maxPrefix]
+    })
     let res = []
-    for (let i = 0; i < queries.length; i++) {
-        let target = queries[i]
+    function getRes(target) {
         let low = 0
-        let high = items.length - 1
+        let high = prefixArr.length - 1
         let ans = 0
         while (low <= high) {
             let mid = Math.floor((low + high) / 2)
-            if (items[mid][0] <= target) {
-                ans = items[mid][1]
+
+            if (prefixArr[mid][0] <= target) {
+                ans = prefixArr[mid][1]
                 low = mid + 1
             } else {
                 high = mid - 1
@@ -27,5 +35,10 @@ var maximumBeauty = function (items, queries) {
         }
         res.push(ans)
     }
+
+    for (let q of queries) {
+        getRes(q)
+    }
     return res
+
 };
