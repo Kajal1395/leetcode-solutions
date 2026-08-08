5746fc9734e97907b4cbfe4c541485baf3f51e4b
Kajal1395 <60035417+Kajal1395@users.noreply.github.com>
2026-08-08 18:05:46 +0530

    Time: 558 ms (13.79%) | Memory: 119.4 MB (65.52%) - LeetSync

---
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.js
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthLargestLevelSum = function (root, k) {
+
+    let queue = []
+    let ans = -1
+    let resSum = []
+
+    queue.push(root)
+    while (queue.length) {
+        let size = queue.length
+        let level = []
+        let children = 0
+        while (children < size) {
+            let node = queue.shift()
+            level.push(node.val)
+            children++
+            if (node.left) queue.push(node.left)
+            if (node.right) queue.push(node.right)
+        }
+        let sum = level.reduce((acc, num) => acc + num, 0)
+        resSum.push(sum)
+    }
+    resSum.sort((a, b) => b - a)
+    if (k <= resSum.length) {
+        ans = resSum[k - 1]
+    }
+    return ans
+};
