45bac2492f1b9dd391ae4784003ca612bba418b1
Kajal1395 <60035417+Kajal1395@users.noreply.github.com>
2026-09-01 14:39:30 +0530

    Time: 7 ms (26.63%) | Memory: 62.5 MB (42.39%) - LeetSync

---
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number} n
+ * @return {TreeNode[]}
+ */
+var generateTrees = function (n) {
+    function dfs(start, end) {
+        
+        if (start > end) return [null]
+        let res = []
+        for (let root = start; root <= end; root++) {
+            let leftSubtree = dfs(start, root - 1)
+            let rightSubtree = dfs(root + 1, end)
+            for (let left of leftSubtree) {
+                for (let right of rightSubtree) {
+                    let node = new TreeNode(root)
+                    node.left = left
+                    node.right = right
+                    res.push(node)
+                }
+            }
+
+        }
+        return res
+    }
+    return dfs(1, n)
+
+};
