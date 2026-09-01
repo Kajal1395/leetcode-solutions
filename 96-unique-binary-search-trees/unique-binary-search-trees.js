4bb64edd2d90a84257f524b0a8da32a8253f6cee
Kajal1395 <60035417+Kajal1395@users.noreply.github.com>
2026-09-01 14:50:30 +0530

    Time: 3 ms (5.63%) | Memory: 54.2 MB (14.08%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.js
+++ 96-unique-binary-search-trees/unique-binary-search-trees.js
@@ -4,23 +4,22 @@
  */
 var numTrees = function (n) {
     let memo = new Map()
-
-    function count(start, end) {
+    function dfs(start, end) {
         if (start > end) return 1
         let key = `${start}${end}`
         if (memo.has(key)) {
             return memo.get(key)
         }
-        let countVal = 0
-        for (let i = start; i <= end; i++) {
-            let left = count(start, i - 1)
-            let right = count(i + 1, end)
-            countVal += (left * right)
+        let res = 0
+        for (let root = start; root <= end; root++) {
+            let left = dfs(start, root - 1)
+            let right = dfs(root + 1, end)
+            res += left * right
         }
-        memo.set(key, countVal)
-        return countVal
-    }
-    return count(1, n)
+        memo.set(key, res)
+        return res
 
+    }
+    return dfs(1, n)
 
 };
