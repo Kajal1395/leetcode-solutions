3951ab16b1924999c6dca3018d24b8a379df98ba
Kajal Pandey <60035417+Kajal1395@users.noreply.github.com>
2026-09-23 19:31:01 +0530

    Time: 89 ms (76.38%) | Memory: 89 MB (68.67%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+    function LCA(root) {
+        if (!root) return null;
+        if (root === p || root === q) return root;
+        let left = LCA(root.left);
+        let right = LCA(root.right);
+        if (!left && !right) {
+            return null;
+        } else if (left && !right) {
+            return left;
+        } else if (!left && right) {
+            return right;
+        } else {
+            return root;
+        }
+    }
+    return LCA(root);
+
+};
